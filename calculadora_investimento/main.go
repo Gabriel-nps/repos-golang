80f09c397478fedc20b8064a0dd0ca758a398db0
Gabriel Paixao <falcomnp@gmail.com>
2024-09-15 11:34:05 -0300

    feat: calculadora-investimento usando input do usuario

--- calculadora_investimento/main.go
+++ calculadora_investimento/main.go
@@ -6,10 +6,23 @@ import (
 )
 
 func main() {
-	var montanteInvestido float64 = 1000
-	var anos float64 = 10
-	taxaRetorno := 5.5
+	var montanteInvestido float64
+	var anos float64
+	var taxaRetorno float64
+	const taxaInflacao = 2.5
 
-	var valorFuturo = montanteInvestido * math.Pow(1+taxaRetorno/100, anos)
-	fmt.Println("Rendimento: R$", valorFuturo)
+	fmt.Print("Investimento: R$")
+	fmt.Scan(&montanteInvestido)
+
+	fmt.Print("Periodo em anos: ")
+	fmt.Scan(&anos)
+
+	fmt.Print("% de retorno: ")
+	fmt.Scan(&taxaRetorno)
+
+	valorFuturo := montanteInvestido * math.Pow(1+taxaRetorno/100, anos)
+	valorFuturoReal := valorFuturo / math.Pow(1+taxaInflacao/100, anos)
+
+	fmt.Println("Rendimento bruto: R$", valorFuturo)
+	fmt.Println("Rendimento com inflacao: R$", valorFuturoReal)
 }
