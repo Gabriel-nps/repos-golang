ecc37dc455e8209e71e1348336b666aefea4d56c
Gabriel Paixao <falcomnp@gmail.com>
2024-09-15 17:03:09 -0300

    refactor: separando logica em funcoes

--- calculadora_investimento/main.go
+++ calculadora_investimento/main.go
@@ -5,26 +5,30 @@ import (
 	"math"
 )
 
+const taxaInflacao = 2.5
+
 func main() {
-	var montanteInvestido float64
-	var anos float64
-	var taxaRetorno float64
-	const taxaInflacao = 2.5
+	montanteInvestido := getValorDoUsuario("Investimento: R$")
+	taxaRetorno := getValorDoUsuario("%  de retorno: ")
+	anos := getValorDoUsuario("Periodo em anos: ")
 
-	fmt.Print("Investimento: R$")
-	fmt.Scan(&montanteInvestido)
-
-	fmt.Print("Periodo em anos: ")
-	fmt.Scan(&anos)
-
-	fmt.Print("%  de retorno: ")
-	fmt.Scan(&taxaRetorno)
-
-	valorFuturo := montanteInvestido * math.Pow(1+taxaRetorno/100, anos)
-	valorFuturoReal := valorFuturo / math.Pow(1+taxaInflacao/100, anos)
+	valorFuturo, valorFuturoReal := calcularValores(montanteInvestido, taxaRetorno, anos)
 
 	txtValorFuturo := fmt.Sprintf("Rendimento bruto: R$%.1f\n", valorFuturo)
 	txtValorFuturoReal := fmt.Sprintf("Rendimento com inflacao: R$%.1f\n", valorFuturoReal)
 
 	fmt.Print(txtValorFuturo, txtValorFuturoReal)
+}
+
+func getValorDoUsuario(texto string) float64 {
+	var valorInserido float64
+	fmt.Print(texto)
+	fmt.Scan(&valorInserido)
+	return valorInserido
+}
+
+func calcularValores(montanteInvestido, taxaRetorno, anos float64) (float64, float64) {
+	valorFuturo := montanteInvestido * math.Pow(1+taxaRetorno/100, anos)
+	valorFuturoReal := valorFuturo / math.Pow(1+taxaInflacao/100, anos)
+	return valorFuturo, valorFuturoReal
 }
